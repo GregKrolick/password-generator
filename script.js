ebf0747a3521373583684a0009ec64db277232e8
Greg Krolick <gregrobk@live.com>
2022-12-05 18:55:38 -0500

    complete

--- script.js
+++ script.js
@@ -33,41 +33,47 @@ function generatePassword() {
   //prompts for password length
   var passwordLength = prompt("Enter Password Length. Must be greater than 7 and less than 129");
   
-  if (passwordLength == null || passwordLength == "" || passwordLength < 8 || passwordLength > 128 ) {
+  if (passwordLength == null || passwordLength == "" || passwordLength < 8 || passwordLength > 128 || Number.isFinite(passwordLength) === false ) {
     passwordLength = parseInt(passwordLength);
     alert("Length criteria not followed. Please try again");
-  }
-
+    return "Error";
+  } else {
   alert("You must confirm at least 2 of 4 following prompts")
 
     if (confirm("Include numbers in password?")) {
     pwNumberChar = true
-    createdPassword += pwNumberChar;
+    createdPassword += numberChars;
     confirmedCharCount ++;
   }
 
   if (confirm("Include lowercase characters in password?")) {
     pwLowerCaseChar = true
-    createdPassword += pwLowerCaseChar;
+    createdPassword += lowerChars;
     confirmedCharCount ++;
   }
 
   if (confirm("Include uppercase characters in password?")) {
     pwUpperCaseChar = true
-    createdPassword += pwUpperCaseChar;
+    createdPassword += upperChars;
     confirmedCharCount ++;
   }
 
   if (confirm("Include special characters in password?")) {
     pwSpecialChar = true
-    createdPassword += pwSpecialChar;
+    createdPassword += specialChars;
     confirmedCharCount ++;
   }
 
-  // confirms if at least 2 character options were selected
+  // confirms if at least 2 character options were selected then generates password
   if (confirmedCharCount < 2){
     alert ("Two character options must be selected")
   } else {
-    console.log("test")
+    var generatedPassword = "";
+    for (var i = 0; i < passwordLength; i++) {
+      var randChar = createdPassword.length * Math.random();
+      generatedPassword += createdPassword.charAt(randChar)
+    }
+    return generatedPassword;
   }
+}
 }
